870386886800408679cb36b22b60bdea1303c2bf
zahraHaghverdii <haghverdiizahraa@gmail.com>
2025-10-07 17:31:10 +0330

    Reducing space

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // هشدار chunk بزرگ رو زیاد کن
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"], // کتابخانه‌ها رو جدا کن
+        },
+      },
+    },
+  },
 });
