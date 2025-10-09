787318b7195318cf2face132206024341597f358
zahraHaghverdii <haghverdiizahraa@gmail.com>
2025-10-09 14:03:33 +0330

    edite base page home

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/Vendor/",
   build: {
     chunkSizeWarningLimit: 1000, // هشدار chunk بزرگ رو زیاد کن
     rollupOptions: {
